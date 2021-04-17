3f359b9df405a5cb3f4758320aba8bf864dcb0fb
conlumina <69419938+conlumina@users.noreply.github.com>
2021-04-18 00:27:26 +0200

    Add files via upload

---
+++ oxygen-repeater-incremental-ids.php
@@ -0,0 +1,51 @@
+<?php
+
+add_filter( 'do_shortcode_tag', function ( $output, $tag, $attr ) {
+
+	if ( in_array($tag, ['oxy_dynamic_list_3']) ) {
+		$query = '//*[@id]/*';
+		$output = rename_element_id($query, $output);
+	}
+
+	return $output;
+
+}, 10, 3);
+
+function rename_element_id($query, $output) {
+
+	# don't spill out warnings
+	libxml_use_internal_errors(true);
+
+	$dom = new DOMDocument();
+	$dom->loadHTML(mb_convert_encoding( $output, 'HTML-ENTITIES', 'UTF-8') );
+	$xpath = new DOMXPath($dom);
+	$elements = $xpath->query($query);
+
+	$counter = 1;
+	$attribute = 'id';
+	$repeater_id = $elements[0]->parentNode->getAttribute($attribute);
+
+	foreach( $elements as $element ) {
+
+		$parent_id = $element->parentNode->getAttribute($attribute);
+
+		if ( $element->hasAttribute($attribute) ) {
+			$new_id = $element->getAttribute($attribute) . '-' . ( ($parent_id == $repeater_id) ? $counter : ($counter - 1) );
+			$element->removeAttribute($attribute);
+	    	$element->setAttribute($attribute, $new_id);
+		}
+
+		if ( $parent_id == $repeater_id ) $counter++;
+	}
+
+	$output = $dom->saveHTML();
+
+	# clear internal error buffer
+	libxml_clear_errors();
+
+	return $output;
+}
+
+
+
+?>
